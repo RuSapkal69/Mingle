2f7e4cd39b8591b684aec6f8489cdd1c1798010a
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-02-04 02:17:08 +0530

    Building the project......

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -1,5 +1,6 @@
 import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
+import toast from "react-hot-toast";
 
 export const useAuthStore = create((set) => ({
     authUser: null,
@@ -35,6 +36,20 @@ export const useAuthStore = create((set) => ({
         }
     },
 
+    login: async (data) => {
+        set({ isLoggingIn: true });
+        try {
+            const res = await axiosInstance.post("/auth/login", data);
+            set({ authUser: res.data });
+            toast.success("Logged in successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in login: ", error);
+        } finally {
+            set({ isLoggingIn: false });
+        }
+    },
+
     logout: async() => {
         try {
             await axiosInstance.post("/auth/logout");
@@ -43,6 +58,20 @@ export const useAuthStore = create((set) => ({
         } catch (error) {
             toast.error("Error logging out");
             console.log("Error in logout: ", error);
+        }
+    },
+
+    updateProfile: async(data) => {
+        set({ isUpdatingProfile: true });
+        try {
+            const res = await axiosInstance.put("/auth/update-profile", data);
+            set({ authUser: res.data });
+            toast.success("Profile updated successfully");
+        } catch (error) {
+            toast.error(error.response.data.message);
+            console.log("Error in updateProfile: ", error);
+        } finally {
+            set({ isUpdatingProfile: false });
         }
     },
 
