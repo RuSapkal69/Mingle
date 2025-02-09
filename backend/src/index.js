f5a06cff9fc7848f92fc625bd8169a335033c071
Rushikesh Sapkal <rushikeshsapkal930@gmail.com>
2025-02-09 23:50:18 +0530

    Builded application structures

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,10 +5,12 @@ import messageRoutes from "./routes/message.route.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { app, server } from "./lib/socket.js";
 
 dotenv.config();
 
-const app = express();
+
+
 const PORT = process.env.PORT;
 
 app.use(express.json({ limit: '10mb' }));
@@ -19,9 +21,9 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log("Server is running on PORT:", PORT);
     connectDB();
 })
--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -2,11 +2,13 @@ import { create } from "zustand";
 import { axiosInstance } from "../lib/axios";
 import toast from "react-hot-toast";
 
-export const useAuthStore = create((set) => ({
+export const useAuthStore = create((set, get) => ({
     authUser: null,
     isSigningUp: false,
     isLoggingIn: false,
     isUpdatingProfile: false,
+    onlineUsers: [],
+    socket: null,
 
     isCheckingAuth: true,
     checkAuth: async() => {
@@ -28,6 +30,7 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/signup", data);
             set({ authUser: res.data });
             toast.success("Account created successfully");
+            get().connectSocket();
         } catch (error) {
             toast.error(error.response.data.message);
             console.log("Error in signup: ", error);
@@ -42,6 +45,8 @@ export const useAuthStore = create((set) => ({
             const res = await axiosInstance.post("/auth/login", data);
             set({ authUser: res.data });
             toast.success("Logged in successfully");
+
+            get().connectSocket();
         } catch (error) {
             toast.error(error.response.data.message);
             console.log("Error in login: ", error);
@@ -74,5 +79,7 @@ export const useAuthStore = create((set) => ({
             set({ isUpdatingProfile: false });
         }
     },
+
+    connectSocket: () => {}
 
 }));
